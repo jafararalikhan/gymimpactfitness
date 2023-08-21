de4fcf2cf19f5267c24be5f0638c9248f365c055
jafar ali khan <jafararalikhan@outlook.com>
2023-08-21 23:18:50 +0530

    SiginUp Page Added

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from home import views
 
 urlpatterns = [
     path('',views.home, name="home"),
+    path('signup',views.signup, name="signup"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def signup(request):
+    return render(request, 'signup.html')
