eb6a5a689742aa46f17cfe382b9191d994d2c72f
jafar ali khan <jafararalikhan@outlook.com>
2023-08-23 11:44:33 +0530

    SinIn page added

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from home import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('signup',views.signup, name="signup"),
+    path('login',views.handlelogin, name="handlelogin"),
+    path('logout',views.handleLogout, name="handleLogout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,61 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def home(request):
     return render(request,'index.html')
 
 def signup(request):
+    if request.method=="POST":
+        username=request.POST.get('usernumber')
+        email=request.POST.get('email')
+        pass1=request.POST.get('pass1')
+        pass2=request.POST.get('pass2')
+    
+        if pass1!=pass2:
+            messages.info(request, "Password is not Matching")
+            return redirect('/signup')
+        
+        
+        try:
+            if User.objects.get(username=username):
+                messages.warning(request, "Phone Number is Taken")
+                return redirect('/signup')
+            
+        except Exception as identifier:
+            pass
+
+        try:
+            if User.objects.get(email=email):
+                messages.warning(request, "Email is Taken")
+                return redirect('/signup')
+        
+        except Exception as identifier:
+            pass
+
+        myuser=User.objects.create_user(username,email,pass1)
+        myuser.save()
+        messages.success(request, "User is Created Please Login")
+        return redirect('/login')
     return render(request, 'signup.html')
+
+def handlelogin(request):
+    if request.method=="POST":        
+        username=request.POST.get('usernumber')
+        pass1=request.POST.get('pass1')
+        myuser=authenticate(username=username,password=pass1)
+        if myuser is not None:
+            login(request,myuser)
+            messages.success(request,"Login Successful")
+            return redirect('/')
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('/login')
+
+    return render(request, 'handlelogin.html')
+
+def handleLogout(request):
+    logout(request)
+    messages.success(request, "Logout Success")
+    return redirect('/login')
