a937f894f086d0126213c5b8e34fad2cd4510ba2
jafar ali khan <jafararalikhan@outlook.com>
2023-09-08 15:13:40 +0530

    Siginup issue solved

--- home/views.py
+++ home/views.py
@@ -10,6 +10,42 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+
+def signup(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('pass1')
+        pass2=request.POST.get('pass2')
+    
+        if pass1!=pass2:
+            messages.info(request, "Password is not Matching")
+            return redirect('/signup')
+        
+        
+        try:
+            if User.objects.get(username=username):
+                messages.warning(request, "Phone Number is Taken")
+                return redirect('/signup')
+            
+        except Exception as identifier:
+            pass
+
+        try:
+            if User.objects.get(email=email):
+                messages.warning(request, "Email is Taken")
+                return redirect('/signup')
+        
+        except Exception as identifier:
+            pass
+
+        myuser=User.objects.create_user(username,email,pass1)
+        myuser.save()
+        messages.success(request, "User is Created Please Login")
+        return redirect('/login')
+    return render(request, 'signup.html')
+ 
+
 def services(request):
     return render(request, 'services.html')
 def profile(request):
@@ -47,40 +83,6 @@ def attendance(request):
         return redirect('/attendance')
     return render(request,"attendance.html",context)
 
-
-def signup(request):
-    if request.method=="POST":
-        username=request.POST.get('usernumber')
-        email=request.POST.get('email')
-        pass1=request.POST.get('pass1')
-        pass2=request.POST.get('pass2')
-    
-        if pass1!=pass2:
-            messages.info(request, "Password is not Matching")
-            return redirect('/signup')
-        
-        
-        try:
-            if User.objects.get(username=username):
-                messages.warning(request, "Phone Number is Taken")
-                return redirect('/signup')
-            
-        except Exception as identifier:
-            pass
-
-        try:
-            if User.objects.get(email=email):
-                messages.warning(request, "Email is Taken")
-                return redirect('/signup')
-        
-        except Exception as identifier:
-            pass
-
-        myuser=User.objects.create_user(username,email,pass1)
-        myuser.save()
-        messages.success(request, "User is Created Please Login")
-        return redirect('/login')
-    return render(request, 'signup.html')
 
 def handlelogin(request):
     if request.method=="POST":        
